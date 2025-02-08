f795713af2c4bde841347b44973b9e29978126f1
oweey-o <79149019+oweey-o@users.noreply.github.com>
2025-02-08 17:26:58 +0700

    Update Prices.cs

--- App/Prices.cs
+++ App/Prices.cs
@@ -4,7 +4,40 @@ public static class Prices
 {
     public static string GetCurrencyAlias(int price, bool isShorNotation, bool isFirstCapital)
     {
-        //TODO напишите тут свое решение
-        throw new NotImplementedException();
+        var n = price % 10;
+        if (!isShorNotation)
+        {
+            switch (n)
+            {
+                case 0:
+                case 5:
+                case 6: 
+                case 7:
+                case 8:
+                case 9:
+                    Console.WriteLine($"{price} рублей");
+                    break;
+                case 1:
+                    Console.WriteLine($"{price} рубль");
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                    Console.WriteLine($"{price} рубля");
+                    break;
+            }
+            for (int i = 11; i < 20; i++)
+            {
+                if (price % 100 == i)
+                {
+                    Console.WriteLine($"{price} рублей");
+                    break;
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine($"{price} руб.");
+        }
     }
 }
